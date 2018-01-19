a2ec6b830762828a04432273e863579fbeba1fa1
bobby3501 <bobby.luisi@gmail.com>
2018-01-19 13:41:36 -0500

    restored orig login. save and branch

--- BobbysMaterialUIapp/src/app/Foot.js
+++ BobbysMaterialUIapp/src/app/Foot.js
@@ -9,14 +9,14 @@ import FlatButton from 'material-ui/FlatButton';
 
 const Foot = () => (
 
-  
+
 
   <AppBar
     style={{position: 'fixed', zIndex: -1, height: 40, bottom: 0, left: 0, backgroundColor: '#bdc3c7', opacity: 0.6}}
     showMenuIconButton={false}
   >
-    <FlatButton style={{position: 'absolute', opacity: 1, left: '25%', right: '25%'}} label="Share Us on Twitter" href={"https://twitter.com/intent/tweet?text=Check%20out%20the%20best%20MIKE%20facts%20at%20%40realMikeFacts.%20You%20can%20make%20your%20own%20real%20MIKE%20facts%20at%20&url=http://realMikeFacts.com"} secondary={true}/>
-    
+    <FlatButton style={{position: 'fixed', opacity: 1, left: '25%', right: '25%'}} label="Share Us on Twitter" href={"https://twitter.com/intent/tweet?text=Check%20out%20the%20best%20MIKE%20facts%20at%20%40realMikeFacts.%20You%20can%20make%20your%20own%20real%20MIKE%20facts%20at%20&url=http://realMikeFacts.com"} secondary={true}/>
+
   </AppBar>
 );
 
