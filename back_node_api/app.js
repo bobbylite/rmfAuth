dcd2052111eda2dac67f4a8b3b73b8a4aa6d5571
bobby3501 <bobby.luisi@gmail.com>
2018-03-29 18:20:07 -0400

    Added security to deny iframe load request

--- back_node_api/app.js
+++ back_node_api/app.js
@@ -37,6 +37,7 @@ app.use('/api', users);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('X-Frame-Options', "SAMEORIGIN");
     next();
 });
 
