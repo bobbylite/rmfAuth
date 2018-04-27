4514c83db8043313ec8c4024992c1f68a47fe21f
bobby3501 <bobby.luisi@gmail.com>
2018-04-27 09:13:26 -0400

    patched react fe

--- front_react/src/app/app.js
+++ front_react/src/app/app.js
@@ -12,7 +12,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-// Render the main app react component into the app div.
+// Render the main app react component into the app div
 // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
 render(<Router>
         <div>
